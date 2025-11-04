137cb8a5182bba955371f0b44e3d6e0e1a25852e
FiekenBleif28 <FiekenBleif28@gmail.com>
2025-11-04 19:23:15 +0800

    Create tailwind + vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="bg-blue-100 min-h-screen w-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600">
+        FrontEnd 
+        <center> .</center>
+      </h1>
+      <h1 className="text-2xl text-red-600">
+         Project aktif
+      </h1>
+    </div>
+  )
+}
+
+export default App
